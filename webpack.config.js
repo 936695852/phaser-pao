66cf20f8912c526eb3a4a0398244cb991e4e8781
王科 <kewangae@isoftstone.com>
2020-12-30 18:42:26 +0800

    oneOf

--- webpack.config.js
+++ webpack.config.js
@@ -21,48 +21,52 @@ module.exports = {
         options: {},
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: [
+        oneOf: [
           {
-            loader: 'style-loader', // 将 JS 字符串生成为 style 节点
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
           {
-            loader: 'css-loader', // 将 CSS 转化成 CommonJS 模块
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
           },
           {
-            loader: 'sass-loader', // 将 Sass 编译成 CSS
+            test: /\.scss$/,
+            use: [
+              {
+                loader: 'style-loader', // 将 JS 字符串生成为 style 节点
+              },
+              {
+                loader: 'css-loader', // 将 CSS 转化成 CommonJS 模块
+              },
+              {
+                loader: 'sass-loader', // 将 Sass 编译成 CSS
+              },
+            ],
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[hash:10].[ext]',
+              outputPath: 'images',
+            },
+          },
+          // 打包其他资源
+          {
+            exclude: /\.(html|js|css|scss|jpg|png|gif)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[hash:5].[ext]',
+              outputPath: 'media',
+            },
           },
         ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8 * 1024,
-          name: '[hash:10].[ext]',
-          outputPath: 'images',
-        },
-      },
-      // 打包其他资源
-      {
-        exclude: /\.(html|js|css|scss|jpg|png|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash:5].[ext]',
-          outputPath: 'media',
-        },
       },
     ],
   },
