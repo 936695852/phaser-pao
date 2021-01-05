28ec54bb82a87b452c1577d784f0f655dc01c70b
王科 <kewangae@isoftstone.com>
2021-01-05 17:44:27 +0800

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,88 @@
 const { resolve } = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'built.js',
-    path: resolve(__dirname, 'dist'),
+  entry: {
+    app: './src/index.js',
   },
-  devtool: 'inline-source-map',
+  // externalsPresets: {
+  //   web: true,
+  // },
+  // externalsType: 'script',
+  // externals: {
+  //   phaser: ['https://cdn.bootcdn.net/ajax/libs/phaser/3.50.0-beta.12/phaser.min.js', 'Phaser'],
+  // },
+  output: {
+    filename: 'js/[name].[contenthash:8].js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/', // 资源引入路径前缀
+    chunkFilename: 'js/[name].[contenthash:8].chunk.js', // 非入口chunk名称
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      // 默认值
+      // minSize: 30 * 1024,
+      // maxSize: 0,
+      // minChunks: 1, // 最少被引用一次
+      // name: true,
+      cacheGroups: {
+        // 分割chunk得组
+        vendors: {
+          // node_modules 文件会被打包到venders组得chunk中， --> venders~xxx.js
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          // 优先级
+          priority: -10,
+          chunks: 'initial'
+        },
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          // 如果重复 就会复用
+          reuseExistingChunk: true,
+        }
+      }
+    },
+    minimizer: [new TerserWebpackPlugin({}), new OptimizeCssAssetsWebpackPlugin({})],
+  },
   // loader的配置
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'eslint-loader', //先执行
-        exclude: /node_modules/,
-        enforce: 'pre',
-        options: {},
-      },
-      {
         oneOf: [
           {
             test: /\.js$/,
-            loader: 'babel-loader',
             exclude: /node_modules/,
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+            include: resolve(__dirname, 'src'),
+            use: [
+              'thread-loader', //开启多进程
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                },
+              },
+            ],
           },
           {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
+            use: [{ loader: MiniCssExtractPlugin.loader }, { loader: 'css-loader' }],
           },
           {
             test: /\.scss$/,
             use: [
+              { loader: MiniCssExtractPlugin.loader },
               {
-                loader: 'style-loader', // 将 JS 字符串生成为 style 节点
-              },
-              {
-                loader: 'css-loader', // 将 CSS 转化成 CommonJS 模块
+                loader: 'css-loader', // 将 CSS 转化成 CommonJS 模块
               },
               {
                 loader: 'sass-loader', // 将 Sass 编译成 CSS
@@ -70,7 +111,17 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      $css: resolve(__dirname, 'src/style'),
+    },
+    extensions: ['.js', '.json'],
+    // modules: [], // 解析模块去哪里找目录
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css', //输出的文件名字
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
@@ -82,10 +133,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: resolve(__dirname, 'src/assets'),
+          from: resolve(__dirname, 'assets'),
           to: 'assets',
         },
       ],
+    }),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(false),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
   ],
   devServer: {
@@ -94,7 +149,7 @@ module.exports = {
     port: 9000,
     open: true,
     hot: true,
+    clientLogLevel: 'none',
+    quiet: true,
   },
-  // mode: 'development',
-  mode: 'production',
 }
